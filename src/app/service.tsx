b9610e969ec90381a7f96049d997dd0ee2f433fd
Huzaifaabdulrab <huzaifaabdulrab2@gmail.com>
2025-03-23 20:18:03 +0500

    Update service.tsx

--- src/app/service.tsx
+++ src/app/service.tsx
@@ -11,8 +11,8 @@ export default function ServicesSection() {
   useEffect(() => {
     AOS.init({
       duration: 1000,
-      once: false, // Animation will only happen once
-      mirror: false, // Animation will not replay when scrolling up
+      once: true,
+      mirror: false,
     });
   }, []);
 
@@ -23,8 +23,9 @@ export default function ServicesSection() {
         "Crafting structured and semantic web pages using HTML, ensuring accessibility, SEO optimization, and seamless integration with modern technologies for better user experience.",
       icon: <FaHtml5 className="text-[#091057] text-5xl mb-4" />,
       animation: "fade-up",
-    },{
-      title: "CSS3 &  Frameworks",
+    },
+    {
+      title: "CSS3 & Frameworks",
       description:
         "Designing responsive and visually appealing layouts using Tailwind CSS and Bootstrap, ensuring efficiency, modern UI, and seamless integration with web technologies.",
       icon: <FaCss3 className="text-[#091057] text-5xl mb-4" />,
@@ -61,26 +62,20 @@ export default function ServicesSection() {
   ];
 
   return (
-    <div className="overflow-hidden">
-      <h1
-        id="Service"
-        className="text-[#091057] font-bold text-4xl text-center pt-10 mt-14"
-      >
+    <div className="overflow-hidden px-4 md:px-10 lg:px-20">
+      <h1 id="Service" className="text-[#091057] font-bold text-4xl text-center pt-10 mt-14">
         Our Services
       </h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-10 px-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-10">
         {services.map((service, index) => (
           <SpotlightCard key={index} className="custom-spotlight-card" spotlightColor="#091057">
-            <div
-              className="shadow-xl rounded-lg p-6 flex flex-col items-center text-center min-h-[310px]"
-              data-aos={service.animation}
-            >
-              <div className=" mb-4 ">{service.icon}</div>
-              <h3 className="text-2xl font-bold text-[#8b8585] lg:w-[70%] w-[100%]">
+            <div className="shadow-xl rounded-lg p-6 flex flex-col items-center text-center min-h-[320px] " data-aos={service.animation}>
+              <div className="mb-4">{service.icon}</div>
+              <h3 className="text-xl font-bold text-[#091057] lg:w-[70%] w-[100%]">
                 {service.title}
               </h3>
-              <p className="mt-4 text-[#353333] font-semibold">
+              <p className="mt-4 text-[#353333] font-medium text-sm md:text-base">
                 {service.description}
               </p>
             </div>
